f4a184a2992260c9d7d41a72952f245be8828bee
Ganpurev99 <ganpurev.d@ebazaar.mn>
2024-10-17 11:27:32 +0800

    cola promo updated and recieved router event changes

--- service/src/routes/cola-promo-list.ts
+++ service/src/routes/cola-promo-list.ts
@@ -1,9 +1,5 @@
 import express, { Request, Response } from "express";
-import axios from "axios";
 import { Product, ProductDoc, Promo } from "@ebazdev/product";
-import { IntegrationCustomerIds } from "../shared/models/integration-customer-ids";
-import { ColaPromoPublisher } from "../events/publisher/cola-promo-created-publisher";
-import { ColaPromoUpdatedPublisher } from "../events/publisher/cola-promo-updated-publisher";
 import { StatusCodes } from "http-status-codes";
 import { natsWrapper } from "../nats-wrapper";
 import {
@@ -13,9 +9,12 @@ import {
   promoTradeshops,
 } from "../shared/models/cola-promo";
 import { BaseAPIClient } from "../shared/utils/cola-api-client";
+import { ColaPromoPublisher } from "../events/publisher/cola-promo-recieved-publisher";
+import { ColaPromoUpdatedPublisher } from "../events/publisher/cola-promo-updated-publisher";
 
 const router = express.Router();
 const colaClient = new BaseAPIClient();
+const colaCustomerId = process.env.COLA_CUSTOMER_ID!;
 
 router.get("/promo-list", async (req: Request, res: Response) => {
   try {
@@ -58,34 +57,31 @@ router.get("/promo-list", async (req: Request, res: Response) => {
       promo.giftProducts = await fetchEbazaarProductIds(promo.colaGiftProducts);
 
       const existingPromo = await Promo.findOne({
-        thirdPartyPromoId: promo.promoid,
+        "thirdPartyData.thirdPartyPromoId": promo.promoid,
       });
 
-      if (existingPromo) {
-        const hasChanges =
-          existingPromo.name !== promo.promoname ||
-          existingPromo.startDate.getTime() !==
-            new Date(promo.startdate).getTime() ||
-          existingPromo.endDate.getTime() !==
-            new Date(promo.enddate).getTime() ||
-          existingPromo.thresholdQuantity !== promo.tresholdquantity ||
-          existingPromo.promoPercent !== promo.promopercent ||
-          existingPromo.giftQuantity !== promo.giftquantity ||
-          existingPromo.isActive !== promo.isactive ||
-          !arraysEqual(existingPromo.products, promo.products) ||
-          !arraysEqual(existingPromo.giftProducts, promo.giftProducts) ||
-          !arraysEqual(existingPromo.tradeshops, promo.colaTradeshops);
+      if (existingPromo && promo.promotypebycode) {
+        const updatedFields = getUpdatedFields(existingPromo, promo);
 
-        if (hasChanges) {
+        if (Object.keys(updatedFields).length > 0) {
           await new ColaPromoUpdatedPublisher(natsWrapper.client).publish({
             id: existingPromo.id.toString(),
+            updatedFields,
+          });
+        }
+      } else {
+        if (
+          (promo.products.length > 0 || promo.giftProducts.length > 0) &&
+          promo.promotypebycode
+        ) {
+          await new ColaPromoPublisher(natsWrapper.client).publish({
             name: promo.promoname,
-            customerId: IntegrationCustomerIds.cocaCola,
+            customerId: colaCustomerId,
             startDate: promo.startdate,
             endDate: promo.enddate,
             thresholdQuantity: promo.tresholdquantity,
-            promoPercent: promo.promopercent,
-            giftQuantity: promo.giftquantity,
+            promoPercent: promo.promopercent ?? 0,
+            giftQuantity: promo.giftquantity ?? 0,
             isActive: promo.isactive,
             tradeshops: promo.colaTradeshops,
             products: promo.products,
@@ -99,27 +95,6 @@ router.get("/promo-list", async (req: Request, res: Response) => {
             colaTradeshops: promo.colaTradeshops,
           });
         }
-      } else {
-        await new ColaPromoPublisher(natsWrapper.client).publish({
-          name: promo.promoname,
-          customerId: IntegrationCustomerIds.cocaCola,
-          startDate: promo.startdate,
-          endDate: promo.enddate,
-          thresholdQuantity: promo.tresholdquantity,
-          promoPercent: promo.promopercent,
-          giftQuantity: promo.giftquantity,
-          isActive: promo.isactive,
-          tradeshops: promo.colaTradeshops,
-          products: promo.products,
-          giftProducts: promo.giftProducts,
-          thirdPartyPromoId: promo.promoid,
-          thirdPartyPromoTypeId: promo.promotypeid,
-          thirdPartyPromoType: promo.promotype,
-          thirdPartyPromoTypeCode: promo.promotypebycode,
-          colaProducts: promo.colaProducts,
-          colaGiftProducts: promo.colaGiftProducts,
-          colaTradeshops: promo.colaTradeshops,
-        });
       }
     }
 
@@ -128,7 +103,7 @@ router.get("/promo-list", async (req: Request, res: Response) => {
     console.error("Cola integration product list get error:", error);
 
     return res.status(StatusCodes.BAD_REQUEST).send({
-      satus: "failure",
+      status: "failure",
     });
   }
 });
@@ -139,10 +114,14 @@ const fetchEbazaarProductIds = async (
   if (thirdPartyIds && thirdPartyIds.length === 0) {
     return [];
   }
+
   const products = (await Product.find({
     "thirdPartyData.productId": { $in: thirdPartyIds },
   }).select("_id thirdPartyData.productId")) as ProductDoc[];
 
+  if (products.length === 0) {
+    return [];
+  }
   return products.map((product) => product._id);
 };
 
@@ -151,6 +130,56 @@ const arraysEqual = (arr1: any, arr2: any) => {
   return arr1.every((value: any, index: any) => {
     return value.toString() === arr2[index].toString();
   });
+};
+
+const getUpdatedFields = (existingPromo: any, promo: any) => {
+  const updatedFields: any = {};
+
+  if (existingPromo.name !== promo.promoname) {
+    updatedFields.name = promo.promoname;
+  }
+
+  if (
+    existingPromo.startDate.getTime() !== new Date(promo.startdate).getTime()
+  ) {
+    updatedFields.startDate = promo.startdate;
+  }
+
+  if (existingPromo.endDate.getTime() !== new Date(promo.enddate).getTime()) {
+    updatedFields.endDate = promo.enddate;
+  }
+
+  if (existingPromo.thresholdQuantity !== promo.tresholdquantity) {
+    updatedFields.thresholdQuantity = promo.tresholdquantity;
+  }
+
+  const promoPercent = promo.promopercent ?? 0;
+  if (existingPromo.promoPercent !== promoPercent) {
+    updatedFields.promoPercent = promoPercent;
+  }
+
+  const giftQuantity = promo.giftquantity ?? 0;
+  if (existingPromo.giftQuantity !== giftQuantity) {
+    updatedFields.giftQuantity = giftQuantity;
+  }
+
+  if (existingPromo.isActive !== (promo.isactive === 1)) {
+    updatedFields.isActive = promo.isactive === 1;
+  }
+
+  if (!arraysEqual(existingPromo.products, promo.products)) {
+    updatedFields.products = promo.products;
+  }
+
+  if (!arraysEqual(existingPromo.giftProducts, promo.giftProducts)) {
+    updatedFields.giftProducts = promo.giftProducts;
+  }
+
+  if (!arraysEqual(existingPromo.tradeshops, promo.totalTradeshops)) {
+    updatedFields.tradeshops = promo.totalTradeshops;
+  }
+
+  return updatedFields;
 };
 
 export { router as colaPromosRouter };
--- service/src/shared/events/cola-promo-updated.event.ts
+++ service/src/shared/events/cola-promo-updated.event.ts
@@ -1,29 +1,20 @@
 import { Types, ObjectId } from "mongoose";
 import { ColaPromoSubjects } from "./cola-promo-event-subjects";
-import { IntegrationCustomerIds } from "../models/integration-customer-ids";
-
 export interface ColaPromoUpdatedEvent {
   subject: ColaPromoSubjects.ColaPromoUpdated;
-
   data: {
     id: string;
-    name: string;
-    customerId: IntegrationCustomerIds;
-    startDate: string;
-    endDate: string;
-    thresholdQuantity: number;
-    promoPercent: number;
-    giftQuantity: number;
-    isActive: boolean;
-    tradeshops: number[];
-    products: ObjectId[];
-    giftProducts: ObjectId[];
-    thirdPartyPromoId: number;
-    thirdPartyPromoTypeId: number;
-    thirdPartyPromoType: string;
-    thirdPartyPromoTypeCode: string;
-    colaProducts: number[];
-    colaGiftProducts: number[];
-    colaTradeshops: number[];
+    updatedFields: {
+      name?: string;
+      startDate?: string;
+      endDate?: string;
+      thresholdQuantity?: number;
+      promoPercent?: number;
+      giftQuantity?: number;
+      isActive?: boolean;
+      tradeshops?: number[];
+      products?: ObjectId[];
+      giftProducts?: ObjectId[];
+    };
   };
 }
