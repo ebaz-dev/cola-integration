4bcf8eae129b6c8d6e31036d419bc8669f763d2b
tsendebna <tsendbayar.o@ebazaar.mn>
2024-11-05 15:37:56 +0800

    merchant debt service

--- service/src/routes/check-merchant-debt.ts
+++ service/src/routes/check-merchant-debt.ts
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.get("/merchant/debt", async (req: Request, res: Response) => {
   try {
-    const { debts } = await checkMerchantDebt(req.query.merchantId as string);
+    const data = await checkMerchantDebt(req.query.merchantId as string);
 
-    return res.status(StatusCodes.OK).send({ data: debts });
+    return res.status(StatusCodes.OK).send({ data });
   } catch (error) {
     console.log("err", error);
     return res.status(StatusCodes.BAD_REQUEST).send({
